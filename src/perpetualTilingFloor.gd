2ed327e5a14758d655f98f1423fbecae8c6c1918
louzyllamma <redstarryan@gmail.com>
2026-05-01 14:50:48 -0500

    Added floorTiling base logic

--- src/perpetualTilingFloor.gd
+++ src/perpetualTilingFloor.gd
@@ -2,16 +2,26 @@ extends StaticBody3D
 
 var activePoint: Vector2 = Vector2(0,0)
 
+@onready var spriteScene = load("res://Scenes/floorSprite.tscn")
+@onready var activeSprites = spriteScene.instantiate()
+@onready var spriteDimension = activeSprites.get_region_rect()
+@onready var lengthOffset = spriteDimension.size.y
+@onready var widthOffset = spriteDimension.size.x
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var spriteDimension = $Sprite3D.get_region_rect()
-	var lengthOffset = spriteDimension.size.y
-	var widthOffset = spriteDimension.size.x
+	pass
+	
+	
 	
 	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	activeSprites.position = Vector3(activePoint.x, 0, activePoint.y)
+	add_child(activeSprites)
+	activePoint.x += 0.01
+	activePoint.y += 0.01
 	delta = delta
 	
